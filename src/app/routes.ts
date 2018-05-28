59ac477a8d46d1f01266e9a361baea2c1342023c
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-05-28 15:51:33 +0600

    some roures is added

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,16 @@
+import { ViewCreditComponent } from './view-credit/view-credit.component';
+import { ViewDebitComponent } from './view-debit/view-debit.component';
+import { HomeComponent } from './home/home.component';
 import { UserInfoComponent } from './user-info/user-info.component';
 import { Routes } from '@angular/router';
 import { UserloginComponent } from './userlogin/userlogin.component';
 import { UserRegisterComponent } from "./user-register/user-register.component";
 
 export const appRoutes: Routes = [
+    // { path: '/', component: HomeComponent },
     { path: 'login', component: UserloginComponent },
     { path: 'register', component: UserRegisterComponent },
-    { path: 'user', component: UserInfoComponent }
+    { path: 'user', component: UserInfoComponent },
+    { path: 'debit', component: ViewDebitComponent },
+    { path: 'credit', component: ViewCreditComponent }
 ];
