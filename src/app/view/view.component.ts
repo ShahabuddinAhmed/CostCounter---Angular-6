ef245b5727f45cfc899746e23e8ee8d11bb14a09
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-05-31 12:34:38 +0600

    temporary function is added'

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,4 +1,7 @@
+import { Subscriber } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../share/user.service';
+import { User } from '../share/user.model';
 
 @Component({
   selector: 'app-view',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view.component.css']
 })
 export class ViewComponent implements OnInit {
-
-  constructor() { }
+  public userData:object = [];
+  constructor(private _userService: UserService) { }
 
   ngOnInit() {
+    this._userService.getUserData()
+      .subscribe(data =>
+        {
+          //console.log(data);
+          this.userData = data;
+          console.log(this.userData);
+        },
+      err=>{
+        console.log(err);
+      });
   }
 
 }
